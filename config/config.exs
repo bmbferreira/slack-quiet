32e7e9eae7764d2b6d7f067abdfa53152b6128fc
Bruno Ferreira <bmibferreira@gmail.com>
2019-03-07 14:36:32 +0000

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,12 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-#
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
 use Mix.Config
 
 # Configures the endpoint
 config :slack_quiet, SlackQuietWeb.Endpoint,
-  url: [host: "localhost"],
+  url: [host: "192.127.0.1"],
   secret_key_base: "CQNu6sCApWrWdBlu/yj+Q/Vydp0D+U0AllMAv/69mWISGjen4EluDDrnmgOXAhPF",
   render_errors: [view: SlackQuietWeb.ErrorView, accepts: ~w(json)],
   pubsub: [name: SlackQuiet.PubSub, adapter: Phoenix.PubSub.PG2]
