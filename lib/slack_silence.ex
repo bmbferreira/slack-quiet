a8c91ca869eb77a6ca9e5ce53d6f9f56b512de78
Bruno Ferreira <bmibferreira@gmail.com>
2019-03-07 15:13:46 +0000

    Update slack_silence.ex

--- lib/slack_silence.ex
+++ lib/slack_silence.ex
@@ -1,13 +1,10 @@
 defmodule SlackQuiet do
   ## TODO !!!!!
-  ## FIXME !!!!
   @moduledoc """
   SlackQuiet keeps the contexts that define your domain
   and business logic.
 ## TODO !!!!!
   ## FIXME !!!!## TODO !!!!!
-  ## FIXME !!!!## TODO !!!!!
-  ## FIXME !!!!
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
