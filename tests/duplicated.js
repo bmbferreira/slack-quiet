93f673a4244b5ed2499794473e4b5b12d9859e95
Bruno Ferreira <bmibferreira@gmail.com>
2019-03-08 14:24:50 +0000

    test

---
+++ tests/duplicated.js
@@ -0,0 +1,59 @@
+var j = 123;
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+
+
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+
+
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+
+
+
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+
+
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
