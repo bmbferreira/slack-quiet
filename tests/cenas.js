d8ba991a2f9c03227a642b8535d157ffc0a0875d
Bruno Ferreira <bmibferreira@gmail.com>
2019-03-12 09:39:25 +0000

    Create cenas.js

---
+++ tests/cenas.js
@@ -0,0 +1,16 @@
+var j = 123;
+while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+
+ while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
+
+
+ while (j < 1) {
+  console.log("hey");
+  j = j - 1; 
+}
